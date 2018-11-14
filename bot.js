9738234185e446732647a0508800871c6583a987
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-14 23:53:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,6 @@ const gif = require("gif-search");
 const client = new Discord.Client({disableEveryone: true});
 
 const prefix = "#";///غير الامر 
-const myID = ['228139766573432832'];
 /////////////////////////
 ////////////////////////
 
